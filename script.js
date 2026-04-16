85087ca2fd71583f49d8dcbb08255ea216c7f66d
lluzel <luzelenam779@gmail.com>
2026-04-15 21:10:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-alert("JS FUNCIONANDO");
 const tipsData = {
     seguridad: "Es normal sentirte en alerta cuando el entorno no es tranquilo. Trata de limitar cuánto consumes noticias negativas y busca espacios donde te sientas seguro, como tu casa o con personas de confianza.",
 
